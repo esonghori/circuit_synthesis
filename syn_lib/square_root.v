40003ded04bde469411ba36bc15ec68195fa805d
siamumar <siam.umar@gmail.com>
2016-07-01 14:30:55 -0700

    Revert "remove one subtraction from square_root_unit"
    This reverts commit 32b74bc6307023d55890e8a52c7f97e4bb5320f6.

--- syn_lib/square_root.v
+++ syn_lib/square_root.v
@@ -7,11 +7,9 @@
 	
 				wire	[N/2-1:0]	Y[0:M];
 				wire	[N/2-1:0]	Y0[0:M];
-				wire	[N/2-1:0]	Y_mid[0:M];
 	
 	assign Y[0] = {1'b1,{(N/2-1){1'b0}}};
 	assign Y0[0] = {1'b1,{(N/2-1){1'b0}}};
-	assign Y_mid[0] = 0;
 	
 	assign O = Y[M];
 	
@@ -23,10 +21,8 @@
 			.x(A),
 			.y_in(Y[gv]), 
 			.y0_in(Y0[gv]),
-			.y_mid_in(Y_mid[gv]),
 			.y(Y[gv+1]), 
-			.y0(Y0[gv+1]),
-			.y_mid(Y_mid[gv+1])
+			.y0(Y0[gv+1])
 		);
 	end
 	endgenerate	
@@ -54,17 +50,15 @@ module square_root_seq #(parameter N = 8, M = N/2)(
 	localparam L = log2(M);
 	
 				reg	[N-1:0]		x;
-				reg	[N/2-1:0]	y_in, y0_in, y_mid_in;
-				wire	[N/2-1:0]	y, y0, y_mid;	
+				reg	[N/2-1:0]	y_in, y0_in;
+				wire	[N/2-1:0]	y, y0;	
 
 	squar_root_unit #(.N(N)) squar_root_unit(
 		.x(x),
 		.y_in(y_in), 
 		.y0_in(y0_in),
-		.y_mid_in(y_mid_in),
 		.y(y), 
-		.y0(y0),
-		.y_mid(y_mid)
+		.y0(y0)
 	);
 	
 	reg [1:0] state;
@@ -78,7 +72,6 @@ module square_root_seq #(parameter N = 8, M = N/2)(
 			x		<= 0;
 			y_in	<= 0;
 			y0_in	<= 0;
-			y_mid_in	<= 0;
 			count	<= 0;
 		end
 		else begin
@@ -91,14 +84,12 @@ module square_root_seq #(parameter N = 8, M = N/2)(
 					x <= A;
 					y_in <= {1'b1,{(N/2-1){1'b0}}};
 					y0_in <= {1'b1,{(N/2-1){1'b0}}};
-					y_mid_in	<= 0;
 					count <= 0;	
 					state <= 2'b10; 					
 				end		
 				2'b10: begin
 					y_in <= y;
 					y0_in <= y0;
-					y_mid_in <= y_mid;
 					count <= count + 1;
 					if (count == M-1) begin
 						ready <= 1'b1;
@@ -117,11 +108,12 @@ endmodule
 
 module squar_root_unit #(parameter N = 8)(
 	input		[N-1:0]		x,
-	input	[N/2-1:0]	y_in, y0_in, y_mid_in,
-	output	[N/2-1:0]	y, y0, y_mid
+	input	[N/2-1:0]	y_in, y0_in,
+	output	[N/2-1:0]	y, y0
 );
 
 	wire 						t;
+	wire		[N/2-1:0]	y_min_y0, y_mid;
 	wire		[N-1:0]		y_sqr;
 	wire		[1:0]		temp;
 	
@@ -130,9 +122,9 @@ module squar_root_unit #(parameter N = 8)(
 	//assign 	t = (y_sqr > x) ? 1 : 0;
 	COMP #(.N(N)) COMP1 (.A(x), .B(y_sqr), .O(t));
 	//assign	y_min_y0 = y_in - y0_in;
-	//SUB_ #(.N(N/2), .M(N/2)) SUB1 (.A(y_in), .B(y0_in), .O({temp[0], y_min_y0}));
+	SUB_ #(.N(N/2), .M(N/2)) SUB1 (.A(y_in), .B(y0_in), .O({temp[0], y_min_y0}));
 	//assign 	y_mid = t? y_min_y0 : y_in;
-	MUX #(.N(N/2)) MUX1 (.A(y_mid_in), .B(y_in), .S(t), .O(y_mid));
+	MUX #(.N(N/2)) MUX1 (.A(y_min_y0), .B(y_in), .S(t), .O(y_mid));
 	assign 	y0 = y0_in >> 1;
 	//assign 	y = y_mid + y0;
 	ADD_ #(.N(N/2), .M(N/2)) ADD1 (.A(y_mid), .B(y0), .O({temp[1],y}));
