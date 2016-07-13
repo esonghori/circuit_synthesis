f7c3d441e608d9589536b1793441a67495288441
siamumar <s2hussai@ieng6-244.ucsd.edu>
2016-07-13 15:24:04 -0700

    new square root implimentation

--- syn_lib/square_root.v
+++ syn_lib/square_root.v
@@ -1,41 +1,54 @@
 `timescale 1ns / 1ps
 
  module square_root_comb #(parameter N = 8, M = N/2)(
-	input 			[N-1:0]		A,
-	output			[N/2-1:0]	O
+	input 	[N-1:0]		A,
+	output	[N/2-1:0]	O
 	);
 	
-				wire	[N/2-1:0]	Y[0:M];
-				wire	[N/2-1:0]	Y0[0:M];
+	wire	[N/2:0]	Y[0:M];
 	
-	assign Y[0] = {1'b1,{(N/2-1){1'b0}}};
-	assign Y0[0] = {1'b1,{(N/2-1){1'b0}}};
-	
-	assign O = Y[M];
+	assign O = Y[M][N/2-1:0];
 	
 	genvar gv;
 	generate
 	for (gv = 0; gv < M; gv = gv + 1)
 	begin: sqr_rt
-		squar_root_unit #(.N(N)) squar_root_unit(
+		squar_root_comb_unit #(.N(N), .K(gv)) squar_root_comb_unit(
 			.x(A),
-			.y_in(Y[gv]), 
-			.y0_in(Y0[gv]),
-			.y(Y[gv+1]), 
-			.y0(Y0[gv+1])
+			.y_in(Y[gv][N/2:N/2-gv]), 
+			.y_out(Y[gv+1][N/2:N/2-gv-1])
 		);
 	end
 	endgenerate	
 	
+endmodule	
+	
+module squar_root_comb_unit #(parameter N = 8, K = 0)(
+	input	[N-1:0]	x,
+	input	[K:0]	y_in,
+	output	[K+1:0]	y_out
+);
+
+	wire 				t, b;
+	wire	[2*K+1:0]	y_sqr;	
+	wire	[K+1:0]		y;
+		
+	assign y = {y_in, 1'b1};		
+	MULT #(.N(K+1), .M(K+1)) MULT1 (.A(y[K:0]), .B(y[K:0]), .O(y_sqr));	
+	COMP #(.N(2*K+2)) COMP1 (.A(x[N-1:N-2*K-2]), .B(y_sqr), .O(t));
+	MUX #(.N(1)) MUX1 (.A(1'b0), .B(1'b1), .S(t), .O(b));		
+	assign y_out = {y_in, b};
+
 endmodule
 
+
 module square_root_seq #(parameter N = 8, M = N/2)(
-	input 							clk,
-	input 							rst,
-	input 							start,
-	input 			[N-1:0]		A,
-	output	reg	[N/2-1:0]	O,
-	output	reg					ready
+	input 				clk,
+	input 				rst,
+	input 				start,
+	input 	[N-1:0]		A,
+	output	[N/2-1:0]	O,
+	output	reg			ready
 	);
 	
 	function integer log2;
@@ -49,85 +62,66 @@ module square_root_seq #(parameter N = 8, M = N/2)(
 	
 	localparam L = log2(M);
 	
-				reg	[N-1:0]		x;
-				reg	[N/2-1:0]	y_in, y0_in;
-				wire	[N/2-1:0]	y, y0;	
+	reg		[N-1:0]	x;
+	reg		[N/2-1:0]	y_in, y0_in;
+	wire	[N/2-1:0]	y_out, y0_out;	
+	reg		[L-1:0]	count;
+	
+	assign O = y_out;
 
-	squar_root_unit #(.N(N)) squar_root_unit(
+	squar_root_seq_unit #(.N(N)) squar_root_seq_unit(
 		.x(x),
 		.y_in(y_in), 
-		.y0_in(y0_in),
-		.y(y), 
-		.y0(y0)
-	);
-	
-	reg [1:0] state;
-	reg [L-1:0] count;
+		.y0_in(y0_in), 
+		.y_out(y_out),
+		.y0_out(y0_out)
+	);		
 	
 	always @(posedge clk or posedge rst) begin
 		if(rst) begin
-			state	<= 2'b00;
-			O		<= 0;
-			ready	<= 0;
 			x		<= 0;
 			y_in	<= 0;
 			y0_in	<= 0;
 			count	<= 0;
+			ready <= 0;
 		end
 		else begin
-			case(state)
-				2'b00: begin
-					state <= {1'b0,start};
-				end		
-				2'b01: begin
-					ready <= 1'b0;
-					x <= A;
-					y_in <= {1'b1,{(N/2-1){1'b0}}};
-					y0_in <= {1'b1,{(N/2-1){1'b0}}};
-					count <= 0;	
-					state <= 2'b10; 					
-				end		
-				2'b10: begin
-					y_in <= y;
-					y0_in <= y0;
-					count <= count + 1;
-					if (count == M-1) begin
-						ready <= 1'b1;
-						O <= y;
-						state <= 2'b00;
-					end
-				end	
-			endcase
+			if(start) begin
+				x		<= A;
+				y_in	<= 0;
+				y0_in <= {1'b1,{(N/2-1){1'b0}}};
+				count <= 0;	
+				ready <= 0;
+			end		
+			else begin
+				y_in	<= y_out;
+				y0_in	<= y0_out;
+				count	<= count + 1;
+				if (count == M-2) begin
+					ready <= 1;
+				end
+			end	
 		end		
-	end
-
-	
+	end	
 	
 endmodule
 
 
-module squar_root_unit #(parameter N = 8)(
-	input		[N-1:0]		x,
+module squar_root_seq_unit #(parameter N = 8)(
+	input	[N-1:0]		x,
 	input	[N/2-1:0]	y_in, y0_in,
-	output	[N/2-1:0]	y, y0
+	output	[N/2-1:0]	y_out, y0_out
 );
 
-	wire 						t;
-	wire		[N/2-1:0]	y_min_y0, y_mid;
-	wire		[N-1:0]		y_sqr;
-	wire		[1:0]		temp;
+	wire				t, b;
+	wire	[N/2-1:0]	y;
+	wire	[N-1:0]		y_sqr;
 	
-	//assign	y_sqr = y_in * y_in;
-	MULT #(.N(N/2), .M(N/2)) MULT1 (.A(y_in), .B(y_in), .O(y_sqr));
-	//assign 	t = (y_sqr > x) ? 1 : 0;
+	assign y = y_in|y0_in;
+	MULT #(.N(N/2), .M(N/2)) MULT1 (.A(y), .B(y), .O(y_sqr));
 	COMP #(.N(N)) COMP1 (.A(x), .B(y_sqr), .O(t));
-	//assign	y_min_y0 = y_in - y0_in;
-	SUB_ #(.N(N/2), .M(N/2)) SUB1 (.A(y_in), .B(y0_in), .O({temp[0], y_min_y0}));
-	//assign 	y_mid = t? y_min_y0 : y_in;
-	MUX #(.N(N/2)) MUX1 (.A(y_min_y0), .B(y_in), .S(t), .O(y_mid));
-	assign 	y0 = y0_in >> 1;
-	//assign 	y = y_mid + y0;
-	ADD_ #(.N(N/2), .M(N/2)) ADD1 (.A(y_mid), .B(y0), .O({temp[1],y}));
+	MUX #(.N(1)) MUX1 (.A(1'b0), .B(1'b1), .S(t), .O(b));	
+	assign y_out = y_in|(y0_in&{(N/2){b}});
+	assign y0_out = y0_in >> 1;
 	
-
-endmodule
+endmodule 
