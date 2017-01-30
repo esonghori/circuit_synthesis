2a1f1546a228fc8631b521ed18340f05130f0927
sadeghriazi <sadeghriazi@gmail.com>
2017-01-30 13:50:25 -0800

    done with the div r q and both

--- div/div_r.v
+++ div/div_r.v
@@ -5,19 +5,19 @@
 // Please add +incdir+$SYNOPSYS/dw/sim_ver+ to your verilog simulator
 // command line (for simulation).
 
-module div(g_input, e_input, o);
+module div_r(g_input, e_input, o);
   parameter width    = 32;
   parameter tc_mode  = 0;
   parameter rem_mode = 1; // corresponds to "%" in Verilog
 
   input  [width-1 : 0] g_input;
   input  [width-1 : 0] e_input;
-  output [2*width-1:0] o;
+  output [width-1 : 0] o;
 
   wire   [width-1 : 0] quotient;
   wire   [width-1 : 0] remainder;
 
-  assign o = {quotient,remainder};
+  assign o = remainder;
   
   // Instance of DW_fp_div
   // more info: https://www.synopsys.com/dw/ipdir.php?c=DW_fp_div
