2f4d87c5a799036427644b8951f7cf6267f099c6
Ebrahim Songhori <e.songhori@gmail.com>
2017-02-08 15:04:09 -0800

    fix argmax

--- argmax/argmax.v
+++ argmax/argmax.v
@@ -1,43 +1,48 @@
 module argmax
 #(
-  parameter S, // number of inputs N = S**2
-  parameter M // input bit-width
+  parameter N, // number of inputs
+  parameter M  // input bit-width
 )
 (
-  input [M*(2**S)-1:0] in,
-  output [M-1:0] max,
-  output [S-1:0] ind
+  in,
+  max,
+  ind
 );
 
+  function integer log2;
+    input [31:0] value;
+    reg [31:0] temp;
+  begin
+    temp = value;
+    for (log2=0; temp>0; log2=log2+1)
+      temp = temp>>1;
+  end
+  endfunction
+
+  localparam S = log2(N);
+
+  input [M*N-1:0] in;
+  output [M-1:0] max;
+  output [S-1:0] ind;
+
+  wire [M-1:0] max_i [N-1:0];
+  wire [S-1:0] ind_i [N-1:0];
+  wire greater [N-1:0];
+
+  assign greater[0] = 0;
+  assign max_i[0] = in[M-1:0];
+  assign ind_i[0] = 0;
+
+  genvar g;
   generate
-  if (S==1) begin:S1
-    assign ind = (in[M-1:0] > in[2*M-1:M])?1'b0:1'b1;
-    assign max = (in[M-1:0] > in[2*M-1:M])?in[M-1:0]:in[2*M-1:M];
-  end else begin:SG1
-    wire [M-1:0] max1, max0;
-    wire [S-2:0] ind1, ind0;
-
-    argmax #(
-      .S(S-1),
-      .M(M)
-    ) X1 (
-      .in(in[(2**S)*M-1:(2**S)*M/2]),
-      .max(max1),
-      .ind(ind1)
-    );
-
-    argmax #(
-      .S(S-1),
-      .M(M)
-    ) X0 (
-      .in(in[(2**S)*M/2-1:0]),
-      .max(max0),
-      .ind(ind0)
-    );
-
-    assign ind = (max0>max1)?{1'b0, ind0}:{1'b1, ind1};
-    assign max = (max0>max1)?max0:max1;
+  for (g=1;g<N;g=g+1) begin:S1
+    assign greater[g] = (max_i[g-1] > in[(g+1)*M-1:g*M]);
+    assign max_i[g] = (greater[g]) ? max_i[g-1]: in[(g+1)*M-1:g*M];
+    assign ind_i[g] = (greater[g]) ? ind_i[g-1]: g;
   end
   endgenerate
+
+  assign max = max_i[N-1];
+  assign ind = ind_i[N-1];
 
 endmodule
